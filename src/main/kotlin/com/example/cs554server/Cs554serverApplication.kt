4bebf84230ea5ffcc0105ba2071045dd2c34a763
Chung0754 <Zhongmohan@gmail.com>
2021-12-13 23:38:15 -0600

    [feature] add itemdatamodel update post request

--- src/main/kotlin/com/example/cs554server/Cs554serverApplication.kt
+++ src/main/kotlin/com/example/cs554server/Cs554serverApplication.kt
@@ -2,66 +2,70 @@ package com.example.cs554server
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.random.Random
 
 
 @SpringBootApplication
 class Cs554serverApplication
 
-var layouts = listOf<Layout>()
-val json_dict: MutableMap<String, String> = mutableMapOf()
-val layouts_json: MutableList<String> = mutableListOf()
-fun main(args: Array<String>) {
-	runApplication<Cs554serverApplication>(*args)
-
-}
 
 data class Component(val id: String, val variant: String, val expiry: String, val itemDataModel: ItemDataModel)
 data class Layout(val pageID: String, val expiry: String, val components: List<Component>)
-data class ItemDataModel(val id: String, val text: String, val color:String, val itemName: String, val imageURL:String,
+data class ItemDataModel(val id: String, var text: String, val color:String, val itemName: String, val imageURL:String,
 						 val iconKey: String, val action: String, val payload: List<String>)
+data class DataModelUpdateRequest(val pageID: String, val id: String)
+
+
+var layouts = listOf<Layout>()
+val json_dict: MutableMap<String, String> = mutableMapOf()
+val layouts_json: MutableList<String> = mutableListOf()
+var page01 = Layout("1", "6000", listOf(
+	Component("3265", "label", "6000",
+		ItemDataModel("3244","this is a label","red","","","","",
+			listOf("payload1","payload2"))
+	),
+	Component("5165", "label", "6000",
+		ItemDataModel("4243","this is another label","green","","","","",
+			listOf())
+	),
+	Component("4765", "button", "6000",
+		ItemDataModel("5249","tap me","red","","","","action1",
+			listOf())
+	),
+	Component("4327", "button", "6000",
+		ItemDataModel("3876","tap me","blue","","","","action2",
+			listOf())
+	),
+	Component("1239", "list", "6000",
+		ItemDataModel("7654","","red","","","","",
+			listOf("item1","item2"))
+	),
+	Component("6645", "icon", "6000",
+		ItemDataModel("9344","sun rise","","","","sunrise","",
+			listOf())
+	)
+))
+
+fun main(args: Array<String>) {
+	json_dict.put("1", page01.toString())
+	runApplication<Cs554serverApplication>(*args)
+
+}
 
 
 @RestController
 class LayoutTest01Resource {
 	@GetMapping(value = ["/LayoutTest01"])
 	fun sampleLayout(): Layout {
-		var layout = Layout("1", "6000", listOf(
-			Component("3265", "label", "6000",
-				ItemDataModel("3244","this is a label","red","","","","",
-					listOf("payload1","payload2"))
-			),
-			Component("5165", "label", "6000",
-				ItemDataModel("4243","this is another label","green","","","","",
-					listOf())
-			),
-			Component("4765", "button", "6000",
-				ItemDataModel("5249","tap me","red","","","","action1",
-					listOf())
-			),
-			Component("4327", "button", "6000",
-				ItemDataModel("3876","tap me","blue","","","","action2",
-					listOf())
-			),
-			Component("1239", "list", "6000",
-				ItemDataModel("7654","","red","","","","",
-					listOf("item1","item2"))
-			),
-			Component("6645", "icon", "6000",
-				ItemDataModel("9344","sun rise","","","","sunrise","",
-					listOf())
-			)
-		))
-
-
-		return layout
+		return page01
 	}
-
-
-
 }
 
 
@@ -131,9 +135,11 @@ class AvailableResouseGetter {
 class LayoutUpload {
 	@PostMapping(value = ["/LayoutUpload"])
 	fun echo(@RequestBody payload: String): String{
-		layouts_json.add(payload)
-		var mappingEndpoint: String = "/LayoutResourse/"+ layouts_json.lastIndex
-		json_dict.put(""+layouts_json.lastIndex, payload)
+		var i = Random.nextInt(0, 9999)
+		while (json_dict.containsKey(""+i))
+			i = Random.nextInt(0, 9999)
+		var mappingEndpoint: String = "/LayoutResourse/"+ i
+		json_dict.put(""+i, payload)
 		return mappingEndpoint
 	}
 }
@@ -152,5 +158,21 @@ class LayoutResourceGetter {
 					"    ]\n" +
 					"    \n" +
 					"}"
+	}
+}
+
+@RestController
+class ItemDataModelUpdate {
+	@PostMapping(value = ["/DataModelUpdate"])
+	fun echo(@RequestBody payload: String): Layout{
+		val jrequest = Json.decodeFromString<DataModelUpdateRequest>(payload)
+		if(json_dict.containsKey(jrequest.pageID)) {
+			var updated = Json.decodeFromString<Layout>(json_dict[jrequest.pageID].toString())
+			for (c in updated.components)
+				if (c.itemDataModel.id == jrequest.id)
+					c.itemDataModel.text += " updated"
+			return updated
+		} else
+			return page01
 	}
 }
